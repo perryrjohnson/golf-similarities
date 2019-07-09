20b9c7b376e9616e467e421896800081478145cc
Perry Johnson <37843586+perryrjohnson@users.noreply.github.com>
2019-07-08 19:57:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,21 @@ import json
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-pgatour_players = tc.SFrame('Data/pgatour_stats.csv')
+scaled_stats = tc.SFrame('Data/scaled_stats.csv')
+raw_stats = tc.SFrame('Data/raw_stats.csv')
 content_model = tc.load_model('player_skill_recommender')
 
 
 app = Flask(__name__)
 
+def get_info(player):
+	data = raw_stats[raw_stats['NAME']==player]
+	return data
+
 
 @app.route('/')
 def home():
-	players = list(pgatour_players['NAME'])
+	players = list(scaled_stats['NAME'])
 	return render_template('choose-player.html', players=players)
 
 @app.route('/scoring-method')
@@ -27,10 +32,16 @@ def scoring():
 @app.route('/make-recommendations', methods=['POST', 'GET'])
 def get_recommendations():
 	player = request.form.get('player-name')
-	if player in list(pgatour_players['NAME']): 
-		recs = content_model.recommend_from_interactions(observed_items=[player], k=5)	
-		your_player = player
-		return render_template('make-recommendations.html', your_player=your_player, recs=recs)		
+	if player in list(scaled_stats['NAME']):
+		recs = content_model.recommend_from_interactions(observed_items=[player], k=5)
+		player_data = []
+		for rec in recs:
+			name = rec['NAME']
+			info = get_info(name)
+			info['score'] = "{:.2}".format(rec['score'])
+			player_data.append(info)
+		your_player=get_info(player)	
+		return render_template('make-recommendations.html', your_player=your_player, recs=player_data)		
 	else:	
 		return render_template('error.html')
 
